a5be62be719bdcbf1494fd02232b56f205df19e0
crazym8nd <>
2025-04-09 12:08:25 +0300

    init commit

---
+++ src/test/java/com/bnm/individuals_api/IndividualsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnm.individuals_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualsApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
