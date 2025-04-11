996b06f7232eea8165cb346e649cbb631e517329
crazym8nd <>
2025-04-11 16:57:52 +0300

    added all teests

--- src/test/java/com/bnm/individuals_api/api/AuthControllerIT.java
+++ src/test/java/com/bnm/individuals_api/api/AuthControllerIT.java
@@ -2,10 +2,13 @@ package com.bnm.individuals_api.api;
 
 import com.bnm.individuals_api.KeycloakTestContainer;
 import com.bnm.individuals_api.dto.UserRegistration;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,7 +23,6 @@ class AuthControllerIT extends KeycloakTestContainer {
   private WebTestClient webTestClient;
 
   @Test
-  @DisplayName("Should return 201 response fore creation of individual")
   public void givenValidIndividualRegistration_whenRegisterIndividual_then201Response() {
 
     // Given
@@ -34,5 +36,108 @@ class AuthControllerIT extends KeycloakTestContainer {
 
     // Then
     result.expectStatus().isCreated();
+  }
+
+  @Test
+  public void givenInValidIndividualRegistration_whenRegisterIndividual_then400Response() {
+
+    // Given
+    final UserRegistration validUserRegistration = new UserRegistration("",
+        "testpassword", "testpassword");
+
+    // When
+    final WebTestClient.ResponseSpec result = webTestClient.post().uri("/v1/auth/registration")
+        .body(Mono.just(validUserRegistration), UserRegistration.class)
+        .exchange();
+
+    // Then
+    result.expectStatus().isBadRequest();
+  }
+
+  @Test
+  public void givenValidIndividualRegistration_whenRepeatRegisterIndividual_then409Response() {
+
+    final UserRegistration validUserRegistration = new UserRegistration("",
+        "testpassword", "testpassword");
+    final WebTestClient.ResponseSpec registration = webTestClient.post().uri("/v1/auth/registration")
+        .body(Mono.just(validUserRegistration), UserRegistration.class)
+        .exchange();
+    registration.expectStatus().isCreated();
+
+    final WebTestClient.ResponseSpec result = webTestClient.post().uri("/v1/auth/registration")
+        .body(Mono.just(validUserRegistration), UserRegistration.class)
+        .exchange();
+
+    result.expectStatus().isEqualTo(HttpStatus.CONFLICT);
+  }
+
+  @Test
+  public void loginShouldReturn200() {
+    // Given
+    final UserRegistration validUserRegistration = new UserRegistration("testEmail@mail.com",
+        "testpassword", "testpassword");
+    WebTestClient.ResponseSpec register = webTestClient.post().uri("/v1/auth/registration")
+        .body(Mono.just(validUserRegistration), UserRegistration.class)
+        .exchange();
+    final var validLoginRequest = validUserRegistration;
+
+    // When
+    WebTestClient.ResponseSpec result = webTestClient.post().uri("/v1/auth/login")
+        .body(Mono.just(validLoginRequest), UserRegistration.class)
+        .exchange();
+
+    // Then
+    result.expectStatus().isOk();
+  }
+
+  @Test
+  public void loginShouldReturn401() {
+    // Given
+    final UserRegistration validUserRegistration = new UserRegistration("testEmail@mail.com",
+        "testpassword", "testpassword");
+    WebTestClient.ResponseSpec register = webTestClient.post().uri("/v1/auth/registration")
+        .body(Mono.just(validUserRegistration), UserRegistration.class)
+        .exchange();
+
+    final var validLoginRequest = new UserRegistration(validUserRegistration.email(),
+        "", "");
+
+    // When
+    WebTestClient.ResponseSpec result = webTestClient.post().uri("/v1/auth/login")
+        .body(Mono.just(validLoginRequest), UserRegistration.class)
+        .exchange();
+
+    // Then
+    result.expectStatus().isUnauthorized();
+  }
+
+  @Test
+  public void refreshToken200() {
+
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void refreshToken401() {
+
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void aboutMe200() {
+
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void aboutMe404() {
+
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void aboutMe401() {
+
+    Assertions.assertTrue(false);
   }
 }
